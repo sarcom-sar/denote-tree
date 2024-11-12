1ffe35f11609c9511a8669a84e0fa15fec114fb8
sarcom-sar <sarsarcom25@gmail.com>
2024-11-12 19:39:50 +0100

    new -edit--fix-current-note, make keywords a list

--- denote-tree.el
+++ denote-tree.el
@@ -409,6 +409,18 @@ Denote wont ask you to confirm it, this is final."
     (apply #'denote-rename-file (mapcar #'cdr denote-tree-edit--current-note)))
   (denote-tree-edit--clean-up))
 
+
+(defun denote-tree-edit--fix-current-note (copy)
+  (let (filetype)
+    (with-temp-buffer
+      (insert-file-contents (alist-get 'file denote-tree-edit--current-note))
+      (goto-char (point-min))
+      (setq filetype (denote-tree--find-filetype (current-buffer))))
+    (setcdr (assq 'keywords copy)
+            (funcall (plist-get filetype :keywords-value-reverse-function)
+                     (cdr (assq 'keywords copy)))))
+  copy)
+
 (defun denote-tree-edit--save-from-widgets (copy)
   ""
   (let ((possible-widgets (mapcar #'widget-at
