bf0ddfa5d7e5665bb419c2fc44d2d0f31b31369f
sarcom-sar <sarsarcom25@gmail.com>
2024-11-14 17:50:07 +0100

    mod denote-tree-edit--after-button, line-aware

--- denote-tree-edit.el
+++ denote-tree-edit.el
@@ -72,13 +72,17 @@ Everything else is still read-only.  All newlines will be dropped.
        #'denote-tree-edit--save-match)
       (denote-tree-edit--widgetize-line))))
 
-
 (defun denote-tree-edit--after-button (pos)
-  "Return position of prop 'button-data in POS."
+  "Return position of prop 'button-data in line POS or nil."
   (save-excursion
     (goto-char pos)
-    (+ (next-single-property-change (line-beginning-position) 'button-data)
-       (length denote-tree-node))))
+    (save-restriction
+      (narrow-to-region (line-beginning-position) (line-end-position))
+      (when-let ((after-button (next-single-property-change
+                                (line-beginning-position)
+                                'button-data)))
+        (+ after-button
+           (length denote-tree-node))))))
 
 (defun denote-tree-edit--widgetize-line ()
   "Make line widgetized."
