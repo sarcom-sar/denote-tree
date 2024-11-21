736d9767e44796ca3fdad47758797d400611c623
sarcom-sar <sarsarcom25@gmail.com>
2024-11-21 22:47:10 +0100

    add tests for denote-tree--clean-up

--- denote-tree-tests.el
+++ denote-tree-tests.el
@@ -16,3 +16,32 @@
            (denote-tree--default-props "a" '(b c))
            (propertize "a" 'denote-tree--type '(b c)))))
 
+(defmacro denote-tree-test--prepare-buffer-space
+    (buffers equal-to visited)
+  "Prepare an environment for testing `denote-tree--clean-up'.
+
+Argument BUFFERS  - starting \"buffers\";
+Argument EQUAL-TO - what BUFFERS should be after the call;
+Argument VISITED  - \"buffers\" to be cleaned up."
+  `(let ((fake-buffers ,buffers)
+         (denote-tree--visited-buffers ,visited)
+         (denote-tree--cyclic-buffers '()))
+     (cl-letf (((symbol-function 'kill-buffer)
+                (lambda (thing)
+                  (if (member thing fake-buffers)
+                      (setq fake-buffers (remove thing fake-buffers))
+                    (error "No buffer named %s" thing))))
+               ((symbol-function 'get-buffer)
+                (lambda (thing)
+                  (member thing fake-buffers))))
+       (denote-tree--clean-up)
+       (should (equal fake-buffers ,equal-to)))))
+
+(ert-deftest denote-tree--clean-up-test ()
+  "Tests for `denote-tree--clean-up'."
+  (denote-tree-test--prepare-buffer-space
+   '(a b c d e f) '(a b c d e f) '())
+  (denote-tree-test--prepare-buffer-space
+   '(a b c d e f) '(b d e f) '(c a))
+  (denote-tree-test--prepare-buffer-space
+   '(a b c d e f) '(a b c d e f) '(g)))
