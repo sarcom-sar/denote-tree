bf967d4fa12b3caa60bd9c3cd39caceab442fc1f
sarcom-sar <sarsarcom25@gmail.com>
2024-12-07 14:48:19 +0100

    bare-bones denote-tree-edit-test.el

---
+++ denote-tree-edit-test.el
@@ -0,0 +1,11 @@
+;;; denote-tree-edit-test.el --- Test denote-tree-edit -*- lexical-binding: t -*-
+
+(require 'denote-tree)
+(require 'denote-tree-edit)
+(require 'ert)
+
+;;; Code:
+
+
+(provide 'denote-tree-edit-test)
+;;; denote-tree-edit-test.el ends here
