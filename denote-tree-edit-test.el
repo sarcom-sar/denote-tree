9d8b80af1702bb89853d68bce5de15de5dede779
sarcom-sar <sarsarcom25@gmail.com>
2024-12-07 20:01:08 +0100

    add tests for denote-tree-edit--widgetize-line

--- denote-tree-edit-test.el
+++ denote-tree-edit-test.el
@@ -109,5 +109,63 @@
       (should (equal dat-struct
                      '("Foo" "Bar"))))))
 
+(ert-deftest denote-tree-edit-test--widgetize-line ()
+  "Tests for `denote-tree-edit--widgetize-line'."
+  (let ((denote-tree-node-description '(title))
+        (denote-tree-edit--current-note '((title . "I am a title")))
+        (denote-tree-edit--current-line 1))
+    (with-temp-buffer
+      (insert "'-"
+              (propertize "* " 'button-data "foo")
+              (propertize "I am a title" 'denote-tree-edit--type 'title)
+              "\n")
+      (denote-tree-edit--widgetize-line)
+      (should (widgetp (widget-at 5)))))
+  (let ((denote-tree-node-description '(title iden))
+        (denote-tree-edit--current-note '((title . "I am a title")
+                                          (iden . "I am iden")))
+        (denote-tree-edit--current-line 1))
+    (with-temp-buffer
+      (insert "'-"
+              (propertize "* " 'button-data "foo")
+              (propertize "I am a title" 'denote-tree-edit--type 'title)
+              " "
+              (propertize "I am a iden" 'denote-tree-edit--type 'iden)
+              "\n")
+      (denote-tree-edit--widgetize-line)
+      (should (widgetp (widget-at 19)))))
+  (let ((denote-tree-node-description '(title "foo" iden))
+        (denote-tree-edit--current-note '((title . "I am a title")
+                                          (iden . "I am iden")))
+        (denote-tree-edit--current-line 1))
+    (with-temp-buffer
+      (insert "'-"
+              (propertize "* " 'button-data "foo")
+              (propertize "I am a title" 'denote-tree-edit--type 'title)
+              " foo "
+              (propertize "I am a iden" 'denote-tree-edit--type 'iden)
+              "\n")
+      (denote-tree-edit--widgetize-line)
+      (should (not (widgetp (widget-at 19))))
+      (should (widgetp (widget-at 23)))))
+  (let ((denote-tree-node-description '(title "foo" iden))
+        (denote-tree-edit--current-note '((title . "I am a title")
+                          (iden . "I am a iden")))
+        (denote-tree-edit--current-line 1))
+    (with-temp-buffer
+      ;; not possible in theory, because `denote-tree-node-description'
+      ;; determines order of arguments
+      (insert "'-"
+              (propertize "* " 'button-data "foo")
+              (propertize "I am a iden" 'denote-tree-edit--type 'iden)
+              " foo "
+              (propertize "I am a title" 'denote-tree-edit--type 'title)
+              "\n")
+      (denote-tree-edit--widgetize-line)
+      (should (equal (widget-get (widget-at 5) :value)
+                     "I am a title"))
+      (should (equal (widget-get (widget-at 23) :value)
+                     "I am a iden")))))
+
 (provide 'denote-tree-edit-test)
 ;;; denote-tree-edit-test.el ends here
