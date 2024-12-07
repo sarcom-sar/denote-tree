4bb04c52a58220ba5562d08dc88fc931ca1f36de
sarcom-sar <sarsarcom25@gmail.com>
2024-12-07 17:01:57 +0100

    add tests for denote-tree-edit-test--prop-match

--- denote-tree-edit-test.el
+++ denote-tree-edit-test.el
@@ -6,6 +6,37 @@
 
 ;;; Code:
 
+(ert-deftest denote-tree-edit-test--prop-match ()
+  "Tests for `denote-tree-edit--prop-match'."
+  (with-temp-buffer
+    (insert "'- "
+            (propertize "* " 'foo 'bar)
+            "A\n"
+            "foos")
+    (should (equal (denote-tree-edit--prop-match 'foo 'bar)
+                   nil))
+    (goto-char 0)
+    (should (equal (denote-tree-edit--prop-match 'foo 'bar)
+                   4)))
+  (should (equal (denote-tree-edit--prop-match "foo" 'bar)
+                 nil))
+  (with-temp-buffer
+    (insert "'-"
+            (propertize "* A " 'foo "title")
+            "A TITLE"
+            "\n")
+    (goto-char 1)
+    (should (equal (denote-tree-edit--prop-match 'foo 'bar)
+                   nil)))
+  (with-temp-buffer
+    (insert "'-"
+            (propertize "* A " 'foo 'bar)
+            "A TITLE"
+            "\n")
+    ;; after propertized bit
+    (goto-char 8)
+    (should (equal (denote-tree-edit--prop-match 'foo 'bar)
+                   nil))))
 
 (provide 'denote-tree-edit-test)
 ;;; denote-tree-edit-test.el ends here
